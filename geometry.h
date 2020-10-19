5bff50b9f5079ca17bb2a77c2b5042f5e7729693
cqfrank <497655640@qq.com>
2020-10-19 18:41:02 +0800

    .

--- geometry.h
+++ geometry.h
@@ -7,13 +7,11 @@
 
 
 template <class t> struct Vec2 {
-	union {
-		struct {t u, v;};
-		struct {t x, y;};
-		t raw[2];
-	};
-	Vec2(): u(0), v(0) {}
-	Vec2(t _u, t _v): u(_u), v(_v) {}
+	t x, y;
+	Vec2(): x(t()), y(t()) {}
+	Vec2(t _x, t _y): x(_x), y(_y) {}
+	Vec2<t>(const Vec2<t> &v): x(t()), y(t()) {*this = v;}
+
 	inline Vec2<t> operator +(const Vec2<t> &V) const {return Vec2<t>(u+V.u, v+V.v);}
 	inline Vec2<t> operator -(const Vec2<t> &V) const {return Vec2<t>(u-V.u, v-V.v);}
 	inline Vec2<t> operator *(float f) 			const {return Vec2<t>(u*f, v*f);}
