20b62dcea3bed7f5627e720f7dadb1a822d126d2
cqfrank <497655640@qq.com>
2020-10-20 18:12:39 +0800

    not finished

--- geometry.h
+++ geometry.h
@@ -1,104 +1,40 @@
 #ifndef __GEOMETRY_H__
 #define __GEOMETRY_H__
-
 #include <cmath>
-#include <ostream>
-#include <vector>
+#include <cassert>
+#include <iostream>
 
+template<size_t DimCols, size_t DimRows, typename T> class mat;
 
-template <class t> struct Vec2 {
-	t x, y;
-	Vec2<t>(): x(t()), y(t()) {}
-	Vec2<t>(t _x, t _y): x(_x), y(_y) {}
-	Vec2<t>(const Vec2<t> &v): x(t()), y(t()) {*this = v;}
-	Vec2<t> & operator =(const Vec2<t> &v) {
-		if (this != &v) {
-			x = v.x;
-			y = v.y;
-		}
-		return *this;
-	}
-
-	Vec2<t> operator +(const Vec2<t> &V) const {return Vec2<t>(x+V.x, y+V.y);}
-	Vec2<t> operator -(const Vec2<t> &V) const {return Vec2<t>(x-V.x, y-V.y);}
-	Vec2<t> operator *(float f) 		 const {return Vec2<t>(x*f, y*f);}
-	t& operator[](const int i) { 
-		if (x <= 0) return x; 
-		else return y;
-	}
-	template <class > friend std::ostream& operator<<(std::ostream& s, Vec2<t>& v);
+template<size_t DIM, typename T> struct vec {
+	vec() {for (size_t i = DIM; i--; data_[i] = T());}
+	T& operator[](const size_t i) {assert(i < DIM); return data_[i];}
+	const T& operator[](const size_t i) const  {assert(i < DIM); return data_[i];}
+private:
+	T data_[DIM];
 };
 
-template <class t> struct Vec3 {
-	t x, y, z;
-	Vec3<t>(): x(t()), y(t()), z(t()) {}
-	Vec3<t>(t _x, t _y, t _z): x(_x), y(_y), z(_z) {}
-	template <class u> Vec3<t>(const Vec3<u> &v);
-	Vec3<t>(const Vec3<t> &v): x(t()), y(t()), z(t()) {*this = v;}
-	Vec3<t> & operator =(const Vec3<t> &v) {
-		if (this != &v) {
-			x = v.x;
-			y = v.y;
-			z = v.z;
-		}
-		return *this;
-	}
-	Vec3<t> operator ^(const Vec3<t> &v) const {return Vec3<t>(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x);}
-	Vec3<t> operator +(const Vec3<t> &v) const {return Vec3<t>(x+v.x, y+v.y, z+v.z);}
-	Vec3<t> operator -(const Vec3<t> &v) const {return Vec3<t>(x-v.x, y-v.y, z-v.z);}
-	Vec3<t> operator *(float f)			const {return Vec3<t>(x*f, y*f, z*f);}
-	t       operator *(const Vec3<t> &v)	const {return x*v.x+y*v.y+z*v.z;}
-	float norm() const {return std::sqrt(x*x+y*y+z*z);}
-	Vec3<t> & normalize(t l=1) {*this = (*this)*(l/norm()); return *this;}
-	t& operator [](const int i) {
-		if (i <= 0) return x;
-		else if (i == 1) return y;
-		else return z;
-	}
-	template <class > friend std::ostream& operator<<(std::ostream& s, Vec3<t>& v);
+template<typename T> struct vec<2, T> {
+	vec(): x(T()), y(T()) {}
+	vec(T X, T Y): x(X), y(Y) {}
+	template <class u> vec<2, T>(const vec<2, u> &v);
+	T& operator[](const size_t i) {assert(i < 2); return i == 0 ? x : y;}
+	const T& operator[](const size_t i) const {assert(i < 2); return i == 0 ? x : y;}
+
+	T x, y;
 };
 
-typedef Vec2<float> Vec2f;
-typedef Vec2<int> Vec2i;
-typedef Vec3<float> Vec3f;
-typedef Vec3<int> Vec3i;
+template<typename T> struct vec<3, T> {
+	vec(): x(T()), y(T()), z(T()) {}
+	vec(T X, T Y, T Z): x(X), y(Y), z(Z) {}
+	template <class u> vec<3, T>(const vec<3, u> &v);
+	T& operator[](const size_t i) {assert(i < 3); return i == 0 ? x : (i == 1 ? y : z);}
+	const T& operator[](const size_t i) const {assert(i < 3); return i == 0 ? x : (i == 1 ? y : z);}
+	float norm() {return std::sqrt(x*x+y*y+z*z);}
+	vec<3, T>& normalize(T l=1) {*this = (*this)*(l/norm()); return *this; }
 
-template <> template <> Vec3<int>::Vec3(const Vec3<float> &v);
-template <> template <> Vec3<float>::Vec3(const Vec3<int> &v);
-
-template <class t> std::ostream& operator<<(std::ostream& s, Vec2<t>& v) {
-	s << "(" << v.x << "," << v.y << ")\n";
-	return s;
-}
-
-template <class t> std::ostream& operator<<(std::ostream& s, Vec3<t>& v) {
-	s << "(" << v.x << "," << v.y << "," << v.z << ")\n";
-	return s;
-}
-
-
-
-const int DEFAULT_ALLOC = 4;
-class Matrix {
-	std::vector<std::vector<float> >m;
-	int rows, cols;
-public:
-	Matrix(int r = DEFAULT_ALLOC, int c = DEFAULT_ALLOC);
-	inline int nrows();
-	inline int ncols();
-
-	static Matrix identity(int dimensions);
-	std::vector<float>& operator[](const int i);
-	Matrix operator*(const Matrix& a);
-	Matrix transpose();
-	Matrix inverse();
-
-	friend std::ostream& operator<<(std::ostream&, Matrix& m);
+	T x, y, z;
 };
 
 
 
-#endif
-
-
-	
